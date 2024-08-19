66be2c62c4a4adb64e83a42e9d75e258e9976879
Thaïs <thais.r@live.fr>
2024-08-19 16:46:48 +0200

    counter corrigé

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import CounterComponent from "@/components/CounterComponent.vue";
+import ListeComponent from "@/components/ListeComponent.vue";
 </script>
 
 <template>
   <CounterComponent />
+  <ListeComponent />
 </template>
 
 <style></style>
